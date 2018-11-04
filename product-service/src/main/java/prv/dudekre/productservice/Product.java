1ee8970ba6126eb66bc98cd253e7aad6a4c337aa
RemigiuszDudek <remigiusz_dudek@tlen.pl>
2018-11-04 22:43:30 +0100

    include productServiceController in ProductServiceApp and change Product.id -> Product.productId

--- product-service/src/main/java/prv/dudekre/productservice/Product.java
+++ product-service/src/main/java/prv/dudekre/productservice/Product.java
@@ -1,18 +1,18 @@
 package prv.dudekre.productservice;
 
 public class Product {
-    private final String id;
+    private final String productId;
     private final String name;
     private final int volume;
 
-    public Product(String id, String name, int volume) {
-        this.id = id;
+    public Product(String productId, String name, int volume) {
+        this.productId = productId;
         this.name = name;
         this.volume = volume;
     }
 
-    public String getId() {
-        return id;
+    public String getProductId() {
+        return productId;
     }
 
     public String getName() {
